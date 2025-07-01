385e93e76b4998d754c18c719aeaa7ef8f87e23f
Alperen Yılan <49797242+Alperenyilan@users.noreply.github.com>
2025-07-01 11:35:15 +0300

    gerekli degisiklikler

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "31fef6f69a655155"; }
+/******/ 		__webpack_require__.h = function() { return "cacfe90c9eaaf746"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
